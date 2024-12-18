8560a371346b675b80719591a4ac1a1ce16f05d6
Alice Chan <alicebay10@yahoo.co.uk>
2024-12-18 10:13:48 +0000

    created a folder 'components' and a file 'HTTPRequests.js & set up and ended the code for the lesson 'Fetching Data with GET part 1.

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HTTPRequests from './components/HTTPRequests';
+
+function App() {
+  return (
+    <div className="App">
+      <HTTPRequests />
+    </div>
+  );
+}
+
+export default App;
